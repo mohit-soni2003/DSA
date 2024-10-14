f30814d690186c51306c1f38ad2c32c5611ca1fa
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-10-14 20:03:09 +0530

    repo setup done

---
+++ TLE/900/tle18.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class javasolu {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int test = scn.nextInt();
+        for (int t = 0; t < test; t++) {
+        }
+    }
+}
